7b5bfbe645b28bd841716034a85b8d90be132f84
xsi60lr <adam.szwarc@siili.com>
2019-12-01 10:37:44 +0100

    refactoring: moved solution to separate function

--- Day1.py
+++ Day1.py
@@ -1,11 +1,13 @@
 
+def basic_fuel_requirement(file):
+    fuel_requirement = 0
+    for line in file.readlines():
+        fuel_requirement += (int(line.rstrip('\n')) // 3 - 2)
+    print(f"Full fuel requirement {fuel_requirement}")
+
 def main(filename):
     with open(filename) as file:
-        fuel_requirement = 0
-        for line in file.readlines():
-            fuel_requirement += (int(line.rstrip('\n')) // 3 - 2)
-        print(f"Full fuel requirement {fuel_requirement}")
-
+        basic_fuel_requirement(file)
 
 if __name__ == "__main__":
     main("Day1Input.txt")
