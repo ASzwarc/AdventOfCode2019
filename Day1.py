25067bebb9c723d7383121484c85bb103b1436de
xsi60lr <adam.szwarc@siili.com>
2019-12-01 11:21:00 +0100

    refactoring: passing list of rows to functions instead of file object

--- Day1.py
+++ Day1.py
@@ -1,13 +1,15 @@
 
-def basic_fuel_requirement(file):
+def basic_fuel_requirement(rows):
     fuel_requirement = 0
-    for line in file.readlines():
+    for line in rows:
         fuel_requirement += (int(line.rstrip('\n')) // 3 - 2)
     print(f"Full fuel requirement {fuel_requirement}")
 
 def main(filename):
     with open(filename) as file:
-        basic_fuel_requirement(file)
+        rows = file.readlines()
+        basic_fuel_requirement(rows)
+        module_full_fuel_requirement(rows)
 
 if __name__ == "__main__":
     main("Day1Input.txt")
