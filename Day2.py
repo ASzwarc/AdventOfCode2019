670c463d4ae9a442b8dc576286a583bb6c66dbff
xsi60lr <adam.szwarc@siili.com>
2019-12-02 18:34:19 +0100

    feature: Day2 - solution for 1st part of challenge.

---
+++ Day2.py
@@ -0,0 +1,25 @@
+
+def main(filename):
+    with open(filename) as file:
+        input_list = [int(x) for x in file.readlines()[0].rstrip().split(",")]
+        ptr = 0
+        input_list[1] = 12
+        input_list[2] = 2
+        while True:
+            opcode = input_list[ptr]
+            if opcode == 1:
+                input_list[input_list[ptr + 3]] = (
+                    input_list[input_list[ptr + 1]] +
+                    input_list[input_list[ptr + 2]])
+            elif opcode == 2:
+                input_list[input_list[ptr + 3]] = (
+                    input_list[input_list[ptr + 1]] *
+                    input_list[input_list[ptr + 2]])
+            elif opcode == 99:
+                print("Finished processing opcode")
+                break
+            ptr += 4
+        print(f"Value at position 0: {input_list[0]}")
+
+if __name__ == '__main__':
+    main("Day2Input.txt")
