a561c1adba887eb244e1958fac72bdf413bd2110
xsi60lr <adam.szwarc@siili.com>
2019-12-02 18:39:06 +0100

    fix: renamed main function and added closing input file after input has been read.

--- Day2.py
+++ Day2.py
@@ -1,25 +1,26 @@
 
-def main(filename):
+def solution_part_one(filename):
+    input_list = []
     with open(filename) as file:
         input_list = [int(x) for x in file.readlines()[0].rstrip().split(",")]
-        ptr = 0
-        input_list[1] = 12
-        input_list[2] = 2
-        while True:
-            opcode = input_list[ptr]
-            if opcode == 1:
-                input_list[input_list[ptr + 3]] = (
-                    input_list[input_list[ptr + 1]] +
-                    input_list[input_list[ptr + 2]])
-            elif opcode == 2:
-                input_list[input_list[ptr + 3]] = (
-                    input_list[input_list[ptr + 1]] *
-                    input_list[input_list[ptr + 2]])
-            elif opcode == 99:
-                print("Finished processing opcode")
-                break
-            ptr += 4
-        print(f"Value at position 0: {input_list[0]}")
+    ptr = 0
+    input_list[1] = 12
+    input_list[2] = 2
+    while True:
+        opcode = input_list[ptr]
+        if opcode == 1:
+            input_list[input_list[ptr + 3]] = (
+                input_list[input_list[ptr + 1]] +
+                input_list[input_list[ptr + 2]])
+        elif opcode == 2:
+            input_list[input_list[ptr + 3]] = (
+                input_list[input_list[ptr + 1]] *
+                input_list[input_list[ptr + 2]])
+        elif opcode == 99:
+            print("Finished processing opcode")
+            break
+        ptr += 4
+    print(f"Value at position 0: {input_list[0]}")
 
 if __name__ == '__main__':
-    main("Day2Input.txt")
+    solution_part_one("Day2Input.txt")
