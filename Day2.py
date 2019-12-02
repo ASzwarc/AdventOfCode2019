d186e05a2f08cb2827b473dbe09a88b75f924f81
xsi60lr <adam.szwarc@siili.com>
2019-12-02 19:22:44 +0100

    feature: Day2 - solution for 2nd part

--- Day2.py
+++ Day2.py
@@ -22,5 +22,35 @@ def solution_part_one(filename):
         ptr += 4
     print(f"Value at position 0: {input_list[0]}")
 
+
+def solution_part_two(filename):
+    input_list = []
+    with open(filename) as file:
+        input_list = [int(x) for x in file.readlines()[0].rstrip().split(",")]
+
+    for noun in range(0, 100):
+        for verb in range(0, 100):
+            ptr = 0
+            test_list = input_list[:]
+            test_list[1] = noun
+            test_list[2] = verb
+            while True:
+                opcode = test_list[ptr]
+                if opcode == 1:
+                    test_list[test_list[ptr + 3]] = (
+                        test_list[test_list[ptr + 1]] +
+                        test_list[test_list[ptr + 2]])
+                elif opcode == 2:
+                    test_list[test_list[ptr + 3]] = (
+                        test_list[test_list[ptr + 1]] *
+                        test_list[test_list[ptr + 2]])
+                elif opcode == 99:
+                    break
+                ptr += 4
+            if test_list[0] == 19690720:
+                result = 100 * noun + verb
+                print(f"Answer: {result}")
+
 if __name__ == '__main__':
     solution_part_one("Day2Input.txt")
+    solution_part_two("Day2Input.txt")
