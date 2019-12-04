ea8b764115b559c2617f481c320683509c9ab0f4
xsi60lr <adam.szwarc@siili.com>
2019-12-04 22:05:46 +0100

    feature: implemented cleaner solution for part 2 of Day 4 challenge. Removed not needed comment from code.

--- Day4.py
+++ Day4.py
@@ -23,24 +23,16 @@ def solution_part_two(range_min, range_max):
         for i in range(len(digit_list) - 1):
             if digit_list[i] > digit_list[i+1]:
                 return False
-        # I don't like this solution.
-        # You could save two indexes: start and end, which would move as group
-        # expands. In the end only size of group needs to be checked:
-        # (end - start) + 1
-        i = 1
-        group_digit = digit_list[0]
-        group_size = 1
-        while i < len(digit_list):
-            if group_digit == digit_list[i]:
-                group_size += 1
-                i += 1
-            elif group_digit != digit_list[i]:
-                if group_size == 2:
+
+        start = 0
+        end = 1
+        while end < len(digit_list):
+            if digit_list[start] != digit_list[end]:
+                if end - start == 2:
                     return True
-                group_digit = digit_list[i]
-                group_size = 1
-                i += 1
-        if group_size == 2:
+                start = end
+            end += 1
+        if end - start == 2:
             return True
         else:
             return False
