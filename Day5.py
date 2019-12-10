f91b07b8f4b57c40803ca3441260478e77371b23
xsi60lr <adam.szwarc@siili.com>
2019-12-10 22:25:26 +0100

    feature: implementation of solution for 1st part of 5th day's challenge. Not working yet.

--- Day5.py
+++ Day5.py
@@ -1,36 +1,49 @@
 def solution_part_one(filename):
-    def analyze_opcode(opcode):
-        parsed_instruction = []
-        opcode_list = [digit for digit in str(opcode)].reverse()
-        oper = int("".join(opcode_list[0:2]))
-
-
     input_list = []
+
+    def analyze_opcode(ptr):
+        opcode_list = [digit for digit in str(input_list[ptr])]
+        opcode_list.reverse()
+        oper = int("".join(opcode_list[0:2]))
+        indexes = []
+        for mode in opcode_list[2:]:
+            ptr += 1
+            if mode == "0": # position mode
+                indexes.append(input_list[ptr])
+            elif mode == "1": # immediate mode
+                indexes.append(ptr)
+            else:
+                print("This shouldn't happen in analyze opcode")
+        if (oper == 1 or oper == 2) and len(indexes) != 3:
+            while len(indexes) < 3:
+                ptr += 1
+                indexes.append(input_list[ptr])
+        return oper, indexes
+
+
     with open(filename) as file:
         input_list = [int(x) for x in file.readlines()[0].rstrip().split(",")]
     input_val = 1
     output_val = None
     ptr = 0
     while True:
-        opcode = input_list[ptr]
-        if opcode == 1:
-            input_list[input_list[ptr + 3]] = (
-                input_list[input_list[ptr + 1]] +
-                input_list[input_list[ptr + 2]])
+        oper, indexes = analyze_opcode(ptr)
+        if oper == 1:
+            input_list[indexes[2]] = (input_list[indexes[0]] +
+                input_list[indexes[1]])
             ptr += 4
-        elif opcode == 2:
-            input_list[input_list[ptr + 3]] = (
-                input_list[input_list[ptr + 1]] *
-                input_list[input_list[ptr + 2]])
+        elif oper == 2:
+            input_list[indexes[2]] = (input_list[indexes[0]] *
+                input_list[indexes[1]])
             ptr += 4
-        elif opcode == 3:
+        elif oper == 3:
             input_list[input_list[ptr + 1]] = input_val
             ptr += 2
-        elif opcode == 4:
+        elif oper == 4:
             output_val = input_list[input_list[ptr + 1]]
             print(f"Opcode 4 at {ptr} OUTPUT is {output_val}")
             ptr += 2
-        elif opcode == 99:
+        elif oper == 99:
             print("Finished processing opcode")
             break
     print(f"Diagnostic code is {output_val}")
