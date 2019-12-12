d91cd7e9710bc1356cfd21952c8c9af96535d5eb
ASzwarc <szwarc.adam@gmail.com>
2019-12-13 00:07:05 +0100

    fix: corrected function for analyzing opcodes in part 2 of day 5 solution. Added test inputs.
    Corrected adding indexes to list in case mode wasn't provided in the opcode. Error was caused due to the fact that there is bigger number of opcodes which vary in number of parameters.

--- Day5.py
+++ Day5.py
@@ -53,19 +53,20 @@ def solution_part_two(input_list, input_val):
             ptr += 1
             if mode == "0": # position mode
                 indexes.append(input_list[ptr])
-            elif mode == "1": # immediate mode
+            else: # immediate mode
                 indexes.append(ptr)
-            else:
-                print("This shouldn't happen in analyze opcode")
-        if (oper == 1 or oper == 2) and len(indexes) != 3:
-            while len(indexes) < 3:
-                ptr += 1
-                indexes.append(input_list[ptr])
+        if (oper == 1 or oper == 2 or oper == 7 or oper == 8):
+            required_length = 3
+        elif oper == 5 or oper == 6:
+            required_length = 2
+        else: # oper == 3 or oper == 4
+            required_length = 1
+
+        while len(indexes) < required_length:
+            ptr += 1
+            indexes.append(input_list[ptr])
         return oper, indexes
 
-    with open(filename) as file:
-        input_list = [int(x) for x in file.readlines()[0].rstrip().split(",")]
-    input_val = 5
     output_val = None
     ptr = 0
     while True:
@@ -124,3 +125,18 @@ if __name__ == '__main__':
         input_list = [int(x) for x in file.readlines()[0].rstrip().split(",")]
     input_val = 1
     solution_part_one(input_list, input_val)
+    # Test input:
+    # input_list = [3,9,8,9,10,9,4,9,99,-1,8] # returns 1 if input is
+                                              # equal to 8, 0 otherwise
+    # input_list = [3,9,7,9,10,9,4,9,99,-1,8] # returns 1 if input is less than
+                                              # 8, 0 otherwise
+    # input_list = [3,3,1108,-1,8,3,4,3,99] # returns 1 if input is
+                                            # equal to 8, 0 otherwise
+    # input_list = [3,3,1107,-1,8,3,4,3,99] # returns 1 if input is less than
+                                            # 8, 0 otherwise
+    # return 0 if the input was zero or 1 if the input was non-zero
+    # input_list = [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]
+    # input_list = [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]
+
+    input_val = 5
+    solution_part_two(input_list, input_val)
