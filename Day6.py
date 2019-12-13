e3c9de4a027557ea1246579eed1f9f5d1b796948
ASzwarc <szwarc.adam@gmail.com>
2019-12-13 01:17:55 +0100

    feature: full and working implementation of day 6 with some test input.

--- Day6.py
+++ Day6.py
@@ -1,18 +1,45 @@
 import re
+import networkx as nx
+from networkx.algorithms import approximation
 
 def parse_input(filename):
     input_list = []
     pattern = re.compile(r"(\S{3})\)(\S{3})")
 
     with open(filename) as file:
-        input_list = [re.match(pattern, line).group(1, 2)
-                      for line in file.readlines()]
-    return input_list
+        unique_orbits = set()
+        for line in file.readlines():
+            orbit_one, orbit_two = re.match(pattern, line).group(1, 2)
+            unique_orbits.add(orbit_one)
+            unique_orbits.add(orbit_two)
+            input_list.append((orbit_one, orbit_two))
+    return input_list, unique_orbits
 
 
-def solution_part_one(input_list):
-    pass
+def solution(input_list, unique_orbits):
+    # port one of the solution
+    graph = nx.DiGraph()
+    graph.add_edges_from(input_list)
+    center_orbit = "COM"
+    orbit_count_checksum = 0
+    for orbit in unique_orbits:
+        result = nx.shortest_path_length(graph, source=center_orbit,
+                                         target=orbit)
+        orbit_count_checksum += result
+        print(f"{center_orbit}->{orbit} = {result} ({orbit_count_checksum})")
+    print(f"ORBIT COUNT CHECKSUM = {orbit_count_checksum}")
+
+    # part two of the solution
+    uni_graph = nx.Graph(graph)
+    result = nx.shortest_path_length(uni_graph, source="YOU", target="SAN") - 2
+    print(f"Minimum number of orbital transfers is {result}")
 
 if __name__ == '__main__':
-    input_list = parse_input("Day6Input.txt")
-    solution_part_one(input_list)
+    input_list, unique_orbits = parse_input("Day6Input.txt")
+    # Test input
+    # input_list = [("COM", "B"), ("B", "C"), ("C", "D"), ("D", "E"), ("E", "F"),
+    #               ("B", "G"), ("G", "H"), ("D", "I"), ("E", "J"), ("J", "K"),
+    #               ("K", "L")]
+    # unique_orbits = set(['COM', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    #                      'K', 'L'])
+    solution(input_list, unique_orbits)
