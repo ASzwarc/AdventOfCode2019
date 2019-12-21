21f77258b8eec62ce65a493cc1dad06b2d54b627
ASzwarc <szwarc.adam@gmail.com>
2019-12-21 21:12:40 +0100

    feature: Day9 - implementation of Intcode computer. Added code for analyzing opcode and reading program from file and from input_list.

---
+++ Day9.py
@@ -0,0 +1,41 @@
+
+class IntcodeComputer():
+    def __init__(self):
+        self._program = []
+        self._ptr = 0
+        self._relative_base = 0
+
+    def read_program_instructions(self, program_list):
+        self._program = program_list[:]
+        self._ptr = 0
+
+    def read_program_from_file(self, filename):
+        with open(filename) as file:
+            input_list = [int(x) for x in
+                          file.readlines()[0].rstrip().split(",")]
+        self.read_program_instructions(input_list)
+
+    def analyze_opcode(self):
+        ptr = self._ptr
+        opcode_list = [digit for digit in str(self._program[ptr])]
+        oper = int("".join(opcode_list[-2::]))
+        indexes = []
+        for mode in opcode_list[-3::-1]:
+            ptr += 1
+            if mode == "0": # position mode
+                indexes.append(self._program[ptr])
+            else: # immediate mode
+                indexes.append(ptr)
+        if (oper == 1 or oper == 2 or oper == 7 or oper == 8):
+            required_length = 3
+        elif oper == 5 or oper == 6:
+            required_length = 2
+        elif oper == 3 or oper == 4 or oper == 9:
+            required_length = 1
+        else: # oper == 99:
+            required_length = 0
+
+        while len(indexes) < required_length:
+            ptr += 1
+            indexes.append(self._program[ptr])
+        return oper, indexes
