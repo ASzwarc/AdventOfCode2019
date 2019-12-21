2887ec7a68687f51e278ac1f60fc6c6ac053b864
ASzwarc <szwarc.adam@gmail.com>
2019-12-21 15:32:48 +0100

    fix: day 12 - changed code to check if all moons in one axis met condition. Still not correct solution.

--- Day12.py
+++ Day12.py
@@ -28,7 +28,7 @@ def solution_part_one(position_list, steps_no):
 def solution_part_two(position_list):
     velocity_list = [[0, 0, 0] for _ in range(4)]
     starting_points_list = deepcopy(position_list)
-    found_steps = [[-1, -1, -1] for _ in range(4)]
+    found_steps = [-1 for _ in range(3)]
     step = 0
     points_to_find = len(position_list)*len(position_list[0])
     while points_to_find > 0:
@@ -45,18 +45,20 @@ def solution_part_two(position_list):
         for moon_no, _ in enumerate(position_list):
             for axis in range(3):
                 position_list[moon_no][axis] += velocity_list[moon_no][axis]
-        for moon_no, moon in enumerate(position_list):
-            for axis_no, axis in enumerate(moon):
+        for axis_no in range(len(position_list[0])):
+            moon_count = 0
+            for moon_no in range(len(position_list)):
                 if ((starting_points_list[moon_no][axis_no] ==
                         position_list[moon_no][axis_no]) and
                     (velocity_list[moon_no][axis_no] == 0) and
-                    (found_steps[moon_no][axis_no] == -1)):
-                    points_to_find -= 1
-                    found_steps[moon_no][axis_no] = step
-                    print(f"Found point for moon {moon_no} and axis {axis_no}"
-                          f" in step {step}")
+                    (found_steps[axis_no] == -1)):
+                    moon_count += 1
+            if moon_count == len(position_list):
+                found_steps[axis_no] = step
+                points_to_find -= 4
+                print(f"Found point for axis {axis_no} in step {step}")
         step += 1
-    unique_steps = list(set([step for row in found_steps for step in row]))
+    unique_steps = list(set(found_steps))
     lcm = unique_steps[0]
     for step in unique_steps[1:]:
         lcm = (lcm * step) // gcd(lcm, step)
@@ -65,14 +67,14 @@ def solution_part_two(position_list):
 
 if __name__ == '__main__':
     # Test input
-    # input_list = [[-1, 0, 2],
-    #               [2, -10, -7],
-    #               [4, -8, 8],
-    #               [3, 5, -1]]
-    input_list = [[-14,  -4, -11],
-                  [ -9,   6,  -7],
-                  [  4,   1,   4],
-                  [  2, -14,  -9]]
+    input_list = [[-1, 0, 2],
+                  [2, -10, -7],
+                  [4, -8, 8],
+                  [3, 5, -1]]
+    # input_list = [[-14,  -4, -11],
+    #               [ -9,   6,  -7],
+    #               [  4,   1,   4],
+    #               [  2, -14,  -9]]
 
     # solution_part_one(input_list, 1000)
     solution_part_two(input_list)
