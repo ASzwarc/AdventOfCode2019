f1a2c5403541eb98773a3067765beec736aa6120
ASzwarc <szwarc.adam@gmail.com>
2019-12-21 21:13:58 +0100

    feature: Day9 - implementation of main loop for reading program. Code not finished yet.

--- Day9.py
+++ Day9.py
@@ -15,7 +15,7 @@ class IntcodeComputer():
                           file.readlines()[0].rstrip().split(",")]
         self.read_program_instructions(input_list)
 
-    def analyze_opcode(self):
+    def _analyze_opcode(self):
         ptr = self._ptr
         opcode_list = [digit for digit in str(self._program[ptr])]
         oper = int("".join(opcode_list[-2::]))
@@ -39,3 +39,77 @@ class IntcodeComputer():
             ptr += 1
             indexes.append(self._program[ptr])
         return oper, indexes
+
+    def less_than(self, indexes):
+        if self._program[indexes[0]] < self._program[indexes[1]]:
+            self._program[indexes[2]] = 1
+        else:
+            self._program[indexes[2]] = 0
+        if self._ptr != indexes[2]:
+            self._ptr += 4
+
+    def equals(self, indexes):
+        if self._program[indexes[0]] == self._program[indexes[1]]:
+            self._program[indexes[2]] = 1
+        else:
+            self._program[indexes[2]] = 0
+        if self._ptr != indexes[2]:
+            self._ptr += 4
+
+    def add(self, indexes):
+        self._program[indexes[2]] = (self._program[indexes[0]] +
+            self._program[indexes[1]])
+        if self._ptr != indexes[2]:
+            self._ptr += 4
+
+    def multiply(self, indexes):
+        self._program[indexes[2]] = (self._program[indexes[0]] *
+            self._program[indexes[1]])
+        if self._ptr != indexes[2]:
+            self._ptr += 4
+
+    def save_input_value(self, input_value):
+            self._program[self._program[self._ptr + 1]] = input_value
+            self._ptr += 2
+
+    def read_output_value(self):
+            output_val = self._program[self._program[self._ptr + 1]]
+            self._ptr += 2
+            return output_val
+
+    def jump_if_true(self, indexes):
+        if self._program[indexes[0]] != 0:
+            self._ptr = self._program[indexes[1]]
+        else:
+            self._ptr += 3
+
+    def jump_if_false(self, indexes):
+        if self._program[indexes[0]] == 0:
+            self._ptr = self._program[indexes[1]]
+        else:
+            self._ptr += 3
+
+    def run_intcode_program(self, input_value):
+        output_val = None
+        while True:
+            oper, indexes = self._analyze_opcode()
+            if oper == 1:
+                self.add(indexes)
+            elif oper == 2:
+                self.multiply(indexes)
+            elif oper == 3:
+                self.save_input_value(input_value)
+            elif oper == 4:
+                output_val = self.read_output_value()
+            elif oper == 5:
+                self.jump_if_true(indexes)
+            elif oper == 6: # jump-if-false
+                self.jump_if_false(indexes)
+            elif oper == 7: # less than
+                self.less_than(indexes)
+            elif oper == 8: # equals
+                self.equals(indexes)
+            elif oper == 99:
+                print(f"Intcode finished! Output: {output_val}")
+                return output_val
+
