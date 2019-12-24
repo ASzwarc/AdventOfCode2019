09cb8090ac63b2ee633beb92d06161ae4dac84c9
ASzwarc <szwarc.adam@gmail.com>
2019-12-24 16:51:54 +0100

    feature: Day22 - reading input from file and empty function for calculation of solution

---
+++ Day22.py
@@ -0,0 +1,13 @@
+def solution_part_one(input_list):
+    def deal_into_new_stack():
+        pass
+    def cut_cards(number):
+        pass
+    def deal_with_increment(number):
+        pass
+
+
+if __name__ == '__main__':
+    with open("Day22Input.txt") as file:
+        result = [line.rstrip('\n') for line in file.readlines()]
+    print(result)
