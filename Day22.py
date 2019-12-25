de3d708a8cb6e89eade260aa91052a595ed98e82
ASzwarc <szwarc.adam@gmail.com>
2019-12-25 21:22:47 +0100

    feature: Day22 - working solution for part one. Added implementation for shuffling operations.

--- Day22.py
+++ Day22.py
@@ -1,29 +1,37 @@
 import re
 
-def solution_part_one(input_list):
-    card_deck = [card for card in range(10007)]
+def solution_part_one(input_list, deck_len):
+    card_deck = [card for card in range(deck_len)]
     deal_into_pattern = re.compile(r"(deal into new stack)")
     cut_cards_pattern = re.compile(r"(cut) (-?\d+)")
     deal_with_pattern = re.compile(r"(deal with increment) (\d+)")
 
     def deal_into_new_stack():
         print("Deal into new stack")
+        card_deck.reverse()
 
     def cut_cards(number):
         print(f"Cut {number} cards")
+        return card_deck[number:] + card_deck[:number]
 
     def deal_with_increment(number):
         print(f"Deal with increment {number}")
+        new_deck = card_deck[:]
+        step = 0
+        for i in range(deck_len):
+            new_deck[step] = card_deck[i]
+            step = (step + number) % deck_len
+        return new_deck
 
     for operation in input_list:
         if deal_into_pattern.match(operation):
             deal_into_new_stack()
         elif cut_cards_pattern.match(operation):
-            number = cut_cards_pattern.match(operation).group(2)
-            cut_cards(number)
+            number = int(cut_cards_pattern.match(operation).group(2))
+            card_deck = cut_cards(number)
         else: # deal with increment
-            number = deal_with_pattern.match(operation).group(2)
-            deal_with_increment(number)
+            number = int(deal_with_pattern.match(operation).group(2))
+            card_deck = deal_with_increment(number)
 
     position_of_2019 = card_deck.index(2019)
     print(f"Position of card 2019 is {position_of_2019}")
@@ -31,4 +39,5 @@ def solution_part_one(input_list):
 if __name__ == '__main__':
     with open("Day22Input.txt") as file:
         result = [line.rstrip('\n') for line in file.readlines()]
-    solution_part_one(result)
+    deck_len = 10007
+    solution_part_one(result, deck_len)
