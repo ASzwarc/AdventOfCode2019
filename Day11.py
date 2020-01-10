83a6c0e4d76479b3de01f6ca27d70c795ddb4073
ASzwarc <szwarc.adam@gmail.com>
2020-01-10 16:40:40 +0100

    feature, refactoring: Day11 - working solution for part one without global variable and with do-while loop

--- Day11.py
+++ Day11.py
@@ -4,32 +4,35 @@ def solution_part_one(filename):
     # convention: (x, y)
     # directions: left, up, right, down
     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-    dir_index = 1
 
-    def turn_robot(position, turn):
-        global dir_index
+    def turn_robot(position, turn, dir_index):
         if turn == 0: #left 90 degrees
             dir_index = (dir_index - 1) & 3
         else: #right 90 degrees
             dir_index = (dir_index + 1) & 3
         return (position[0] + directions[dir_index][0],
-                position[1] + directions[dir_index][1])
+                position[1] + directions[dir_index][1]), dir_index
 
     comp = IntcodeComputer()
     visited_positions = {}
+    dir_index = 1
     position = (0, 0)
     comp.read_program_from_file(filename)
-    colored_panels_no = 0
 
-    while not comp._is_finished:
-        color_of_panel = visited_positions.get(position, default=0)
-        new_color, turn = comp.run_intcode_program(color_of_panel)
+    while True:
+        color_of_panel = visited_positions.get(position, 0)
+        new_color = comp.run_intcode_program(color_of_panel)
+        turn = comp.run_intcode_program()
+
+        if comp._is_finished:
+            break
+
         if new_color != color_of_panel:
-            colored_panels_no += 1
+            visited_positions[position] = new_color
+        position, dir_index = turn_robot(position, turn, dir_index)
 
-        position = turn_robot(position, turn)
 
-    print(f"Number of colored panels is {colored_panels_no}")
+    print(f"Number of colored panels is {len(visited_positions)}")
 
 if __name__ == '__main__':
     solution_part_one("Day11Input.txt")
