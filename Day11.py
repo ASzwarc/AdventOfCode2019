1d2dfb1856ea7209fb6c1e46ed08b6aea89345cb
ASzwarc <szwarc.adam@gmail.com>
2020-01-11 01:10:27 +0100

    refactoring: Day11 - extracted functionality from solution_part_one so it can be reused for part two
    Function turn_robot is now global function and main routine for running intcode and painting of tiles have been moved to function run_painting_program.

--- Day11.py
+++ Day11.py
@@ -1,20 +1,20 @@
 from IntcodeComputer import IntcodeComputer
 
-def solution_part_one(filename):
+def turn_robot(position, turn, dir_index):
     # convention: (x, y)
     # directions: left, up, right, down
     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
-    def turn_robot(position, turn, dir_index):
-        if turn == 0: #left 90 degrees
-            dir_index = (dir_index - 1) & 3
-        else: #right 90 degrees
-            dir_index = (dir_index + 1) & 3
-        return (position[0] + directions[dir_index][0],
-                position[1] + directions[dir_index][1]), dir_index
+    if turn == 0: #left 90 degrees
+        dir_index = (dir_index - 1) & 3
+    else: #right 90 degrees
+        dir_index = (dir_index + 1) & 3
+    return (position[0] + directions[dir_index][0],
+            position[1] + directions[dir_index][1]), dir_index
 
+def run_painting_program(filename, starting_color):
     comp = IntcodeComputer()
-    visited_positions = {}
+    visited_positions = {(0,0): starting_color}
     dir_index = 1
     position = (0, 0)
     comp.read_program_from_file(filename)
@@ -30,9 +30,11 @@ def solution_part_one(filename):
         if new_color != color_of_panel:
             visited_positions[position] = new_color
         position, dir_index = turn_robot(position, turn, dir_index)
+    return visited_positions
 
-
-    print(f"Number of colored panels is {len(visited_positions)}")
+def solution_part_one(filename):
+    painted_positions = run_painting_program(filename, 0)
+    print(f"Number of colored panels is {len(painted_positions)}")
 
 if __name__ == '__main__':
     solution_part_one("Day11Input.txt")
