6f1250a32836cce647d8327c22334a2693e444a7
ASzwarc <szwarc.adam@gmail.com>
2020-01-16 16:37:39 +0100

    feature: Day10 - working implementation for part one
    Previous, not finished implementation has been completely removed. Reading input has been changed so that coordinates of asteroids are passed. Solutions has been changed - now atan2 between asteroids is calculated and added to set. If asteroids are inline then value won't be added to set. Added parsing and printing solution for test input.

--- Day10.py
+++ Day10.py
@@ -1,23 +1,27 @@
 import math
 
 def solution_part_one(filename):
+    asteroid_pos = []
     with open(filename) as file:
-        asteroid_map = [[sign for sign in input_line.rstrip()] for input_line
-                        in file.readlines()]
-    result = get_asteroids_visibility(asteroid_map)
-    return max(result, key=lambda k: result[k])
+        for row_no, row in enumerate(file.readlines()):
+            for col_no, col in enumerate(row.rstrip()):
+                if col == "#":
+                    asteroid_pos.append((col_no, row_no))
+    print(get_asteroids_visibility(asteroid_pos))
 
-def distance(point1, point2):
-    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
 
-def is_between(point_start, point_between, point_end):
-    return (distance(point_start, point_between)
-            + distance(point_between, point_end)
-            == distance(point_start, point_end))
-
-def get_asteroids_visibility(asteroids_map):
-    asteroids_visibility = {}
-    return asteroids_visibility
+def get_asteroids_visibility(asteroids_pos):
+    best_score = 0
+    best_x, best_y = -1, -1
+    for x, y in asteroids_pos:
+        visible_asteroids = set()
+        for x2, y2 in asteroids_pos:
+            if (x, y) != (x2, y2):
+                visible_asteroids.add(math.atan2(y2 - y, x2 - x))
+        if len(visible_asteroids) > best_score:
+            best_score = len(visible_asteroids)
+            best_x, best_y = x, y
+    return best_score, (best_x, best_y)
 
 
 if __name__ == '__main__':
@@ -33,6 +37,8 @@ if __name__ == '__main__':
         ['#', '#', '.', '.', '.', '#', '.', '.', '#', '.'],
         ['.', '#', '.', '.', '.', '.', '#', '#', '#', '#']
     ]
+    positions = [(x, y) for y, row in enumerate(test_input_map)
+                 for x, elem in enumerate(row) if elem == '#']
     # Result: best should be (5, 8) with 33 asteroids detected
-    print(get_asteroids_visibility(test_input_map))
+    print(get_asteroids_visibility(positions))
     solution_part_one("Day10Input.txt")
