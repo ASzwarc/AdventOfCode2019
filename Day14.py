75a7bb805343ff87e189ac496bc53bcf285cf55c
ASzwarc <szwarc.adam@gmail.com>
2020-01-26 17:34:40 +0100

    feature: Day14 - added simplest testcase from challenge

--- Day14.py
+++ Day14.py
@@ -11,4 +11,12 @@ def parse_input_file(filename):
     return input_dict
 
 if __name__ == '__main__':
-    print(parse_input_file("Day14Input.txt"))
+    # Simple testing input
+    test_input1 = {'2 A': '9 ORE',
+                   '3 B': '8 ORE',
+                   '5 C': '7 ORE',
+                   '1 AB': '3 A, 4 B',
+                   '1 BC': '5 B, 7 C',
+                   '1 CA': '4 C, 1 A',
+                   '1 FUEL': '2 AB, 3 BC, 4 CA'}
+    assert(165 == solution_part_one(test_input1))
