53bfd76901ba63b3ad1858d167eebb54866f125a
ASzwarc <szwarc.adam@gmail.com>
2020-02-03 17:41:40 +0100

    refactoring: Day14 - completely new solution for part one that's using queue instead of recursion
    First test is passing, second one not.

--- Day14.py
+++ Day14.py
@@ -11,37 +11,42 @@ def parse_input_file(filename):
     return input_dict
 
 def solution_part_one(input_dict):
-    def get_basic_ingredients_for(name, count, basic_formula):
-        formula = reactions.get(name)
-        for elem, quan in formula['ing'].items():
-            if len(reactions.get(elem)['ing']) == 1:
-                print(f"Adding {quan} {elem}")
-                if elem in basic_formula:
-                    basic_formula[elem] += quan * count
-                else:
-                    basic_formula[elem] = quan * count
-            else:
-                print("Searching ingredients for " + elem)
-                get_basic_ingredients_for(elem, quan, basic_formula)
-
     reactions = {}
     for key, value in input_dict.items():
         reactions[key.split()[1]] = {'no': int(key.split()[0]),
             'ing': {e.split()[1]: int(e.split()[0]) for
                     e in value.split(', ')}}
-    basic_formula = {}
-    get_basic_ingredients_for('FUEL', 1, basic_formula)
-    ore_count = 0
-    for key, value in basic_formula.items():
-        a = reactions[key]['no']
-        b = reactions[key]['ing']['ORE']
-        mod = value % a
-        if mod == 0:
-            ore_count += (value // a) * b
-        else:
-            ore_count += ((value // a) + 1) * b
-    return ore_count
 
+    reactions_queue = [('FUEL', 1)]
+    basic_ingredients = {}
+    while reactions_queue:
+        reaction = reactions_queue.pop(0)
+        formula = reactions.get(reaction[0])
+        print(f"{reaction} -> {formula}")
+        for element, count in formula['ing'].items():
+            if element == 'ORE':
+                if reaction[0] in basic_ingredients:
+                    basic_ingredients[reaction[0]] += reaction[1]
+                else:
+                    basic_ingredients[reaction[0]] = reaction[1]
+            else:
+                reactions_queue.append((element, count*reaction[1]))
+    ore_count = 0
+    for element, count in basic_ingredients.items():
+        formula = reactions.get(element)
+        no = formula['no']
+        if count > no:
+            a = count // no
+            if count % no != 0:
+                a += 1
+        else:
+            a = no
+        a *= formula['ing']['ORE']
+        print(f"For {count} {element} adding {a} ORE")
+        ore_count += a
+    print(basic_ingredients)
+    print(ore_count)
+    return ore_count
 
 if __name__ == '__main__':
     # Simple testing input
